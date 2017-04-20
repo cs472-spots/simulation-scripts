d75d39a21b21ca7b55133f2171aaf04e6199690a
Martin Jaime <martin.jaime-viveros@originate.com>
2017-04-19 17:09:51 -0700

    correctly prints json object with spots info

--- make-spots.py
+++ make-spots.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python
 import sys
 
+def write(s):
+    sys.stdout.write(s)
+
+def writeSpot(spot, indent):
+    write('{indent}"{label}" : false,\n'.format(indent=indent*2, label='authorized'))
+    write('{indent}"{label}" : "",\n'.format(indent=indent*2, label='occupant'))
+    write('{indent}"{label}" : "staff",\n'.format(indent=indent*2, label='type'))
+    write('{indent}"{label}" : "false",\n'.format(indent=indent*2, label='vacancy'))
+    write('{indent}"{label}" : {lat},\n'.format(indent=indent*2, label='latitude', lat=spot[0]))
+    write('{indent}"{label}" : {lon}\n'.format(indent=indent*2, label='longitude', lon=spot[1]))
+
+def writeLot(f, spots):
+    write("{\n")
+    spotsLen = len(spots)
+
+    for spotNumber, spot in zip(range(spotsLen), spots):
+        write('  "{:0>4}" : {{\n'.format(spotNumber))
+        writeSpot(spot, '  ')
+        write('  }}{}\n'.format("," if spotNumber < spotsLen-1 else ""))
+
+    write("}\n")
+
 spots = [
         (36.106795, -115.143744),
         (36.106818, -115.143744),
@@ -17,3 +39,4 @@ filename = sys.argv[1]
 
 spotsFile = open(filename, 'w')
 
+writeLot(spotsFile, spots)
